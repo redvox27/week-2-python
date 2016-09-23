bfa35420395b197c69907faa1d4daaf74c92ee48
redvox27 <v.t.luder@st.hanze.nl>
2016-09-23 10:02:49 +0200

    opdracht tien stap 2 beginnen

--- opgave10.py
+++ opgave10.py
@@ -6,11 +6,25 @@
 
 validCard = "4388576018410707" #16 digits
 invalidCard = "4388576018402626" #16 digits
-
+unevenList = []
+evenList = []
 def validCheck(cardNumber):
     n = 1
     cardNumber = cardNumber[::-1]
     cardNumber =[cardNumber[i:i + n] for i in range(0, len(cardNumber), n)]
     print(cardNumber)
+    cardNumber = [int(i) for i in cardNumber]
+    print(cardNumber)
+
+    unevenList = cardNumber[1::2] #start, stop, step // de start is eerste index, stop = einde list, step = 2 dus de uneven indexes
+    print("unevenList: ",unevenList)
+
+    evenList = cardNumber[0::2]
+    print("evenList: ", evenList)
+
+    unevenResult = sum(unevenList) #som van values in list
+    print(unevenResult)
+
+
 
 validCheck(validCard)
